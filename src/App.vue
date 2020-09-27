bad4c41a1019c689286889f36aba41ad7d7e6e17
harmyderoman <romangarmider@gmail.com>
2020-09-27 17:11:47 +0300

    app logic

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>Vue 3</h1>
+  <div class="container">
+    <PostCard
+      v-for="cat in cats"
+      :id="cat.id"
+      :name="cat.username"
+      :text="cat.company.catchPhrase"
+      :key="cat.id"
+      :website="cat.website"
+    />
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import PostCard from "./components/PostCard";
+
 export default {
   name: "App",
-  components: {},
+  components: { PostCard },
+  data() {
+    return {};
+  },
+  setup() {
+    const cats = ref([]);
+    const fetchCats = async () => {
+      cats.value = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      ).then((response) => response.json());
+    };
+    return {
+      cats,
+      fetchCats,
+    };
+  },
+  mounted() {
+    this.fetchCats();
+  },
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  height: 800px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: space-around;
+  flex-wrap: wrap;
+  padding: 20px 100px;
+}
+</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <img :src="imgUrl" alt="cat pic" />
+    <h1>{{ catName }}</h1>
+    <p>{{ text }}</p>
+    <p>{{ website }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    id: String,
+    name: String,
+    text: String,
+    website: String,
+  },
+  setup(props) {
+    const imgUrl = computed(
+      () => `https://robohash.org/${props.id}?set=set4&size=180x180`
+    );
+    const catName = computed(() => props.name.replace(/[_.-]/gi, " "));
+    return {
+      imgUrl,
+      catName,
+    };
+  },
+  computed: {
+    //   imgUrl() {
+    //     return `https://robohash.org/${this.id}?set=set4&size=180x180`;
+    //   },
+    // catName() {
+    //   return this.name.replace(/[_.-]/gi, " ");
+    // },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #cecece;
+}
+p {
+  word-wrap: normal;
+}
+</style>
